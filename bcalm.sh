dea7a97e92a37abc752ed6364f87e7ce5898832d
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2017-12-20 06:00:34 -0800

    delete all files

--- bcalm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#! $1: kmerSize | $2: abundance | $3: runNo | $4: seqFileName | $5: SeqType (fa,fq,fastaq)
-./bcalm/build/bcalm -in data/$4.$5 -kmer-size $1 -abundance-min $2
-mkdir temp
-mv $4.* temp
-python bcalm/scripts/convertToGFA.py temp/$4.unitigs.fa $4_$1_$2.gfa $1
-mv *.gfa GFAs
-rm -r temp
--- run.sh
+++
@@ -1,3 +0,0 @@
-python phase1_bcalm.py $1
-python phase2_bandage.py
-python phase3_plot.py
